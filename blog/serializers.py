e7e321fc2eeeab41d7268b94395fb40164c82737
Amrit5s2002 <guramrit1066@gmail.com>
2024-04-27 13:31:06 +0530

    frontend done

--- blog/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Blog
-
-class BlogSerializer(ModelSerializer):
-    class Meta():
-        model = Blog
-        fields = '__all__'
